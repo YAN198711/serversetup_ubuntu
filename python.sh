499536d05c9e8396d850d16a6dca05065b802b61
YAN198711 <49545154+YAN198711@users.noreply.github.com>
2021-02-12 00:40:38 +0900

    Update python.sh

--- python.sh
+++ python.sh
@@ -2,12 +2,13 @@
 cd ~
 
 # pyenvのインストール
-sudo apt -y install build-essential libffi-dev libssl-dev zlib1g-dev liblzma-dev libbz2-dev libreadline-dev libsqlite3-dev # bz2, readline, sqlite3
+yes | sudo apt install build-essential libffi-dev libssl-dev zlib1g-dev liblzma-dev libbz2-dev libreadline-dev libsqlite3-dev
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="${PYENV_ROOT}/bin:${PATH}"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 source ~/.bashrc
+exec $SHELL -l
 pyenv install 3.8.6
 pyenv global 3.8.6
 pyenv rehash
