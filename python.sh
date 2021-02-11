af4c7fc53a0da632f9de996095f73185cb0fbdbb
YAN198711 <49545154+YAN198711@users.noreply.github.com>
2021-02-12 00:33:40 +0900

    Update python.sh

--- python.sh
+++ python.sh
@@ -5,13 +5,13 @@ cd ~
 sudo apt -y install build-essential libffi-dev libssl-dev zlib1g-dev liblzma-dev libbz2-dev libreadline-dev libsqlite3-dev # bz2, readline, sqlite3
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="${PYENV_ROOT}/bin:${PATH}"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 source ~/.bashrc
-#pyenv -v
 pyenv install 3.8.6
 pyenv global 3.8.6
 pyenv rehash
+# pyenv -v
 
 # pipインストール
 pip install --upgrade pip
