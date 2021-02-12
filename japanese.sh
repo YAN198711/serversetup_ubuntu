0cdf8e74f8e6297d81467a9208e7888933a1814a
YAN198711 <49545154+YAN198711@users.noreply.github.com>
2021-02-13 00:35:42 +0900

    Update japanese.sh

--- japanese.sh
+++ japanese.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd ~
+sudo apt update
 
 # 日本語化
-sudo apt -y install language-pack-ja-base language-pack-ja ibus-mozc
+yes | sudo apt install language-pack-ja-base language-pack-ja ibus-mozc
 sudo localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 source /etc/default/locale
