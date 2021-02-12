d34e3e777fec735afbb2490f839e306787931b6f
YAN198711 <49545154+YAN198711@users.noreply.github.com>
2021-02-13 00:38:50 +0900

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -17,3 +17,12 @@ sudo systemctl enable xrdp
 # ポートの有効化
 sudo ufw allow from any to any port 3389 proto tcp
 yes | sudo ufw enable
+
+# WINEのインストール
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+sudo apt update
+yes | sudo apt install --install-recommends winehq-stable
+# wine --version
