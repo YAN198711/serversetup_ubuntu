da0079ea3afb30553ded447a318f127280c72b0a
YAN198711 <49545154+YAN198711@users.noreply.github.com>
2021-02-13 00:33:52 +0900

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 cd ~
+sudo apt update
 
 # MATEのインストール
-sudo apt install ubuntu-desktop
-sudo apt install mate-* -y
-sudo apt install ubuntu-mate-* -y
+yes | sudo apt install ubuntu-desktop
+yes | sudo apt install mate-*
+yes | sudo apt install ubuntu-mate-*
 echo mate-session > ~/.xsession
 
 # リモートデスクトップのインストール
-sudo apt install xrdp -y
+yes | sudo apt install xrdp
 sudo update-rc.d -f xrdp defaults
 sudo service xrdp restart
 sudo systemctl enable xrdp
 
 # ポートの有効化
 sudo ufw allow from any to any port 3389 proto tcp
+yes | sudo ufw enable
