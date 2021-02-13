ac5484d54d83744acf3ce6c458dd5689c218410c
YAN198711 <49545154+YAN198711@users.noreply.github.com>
2021-02-13 18:46:31 +0900

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -2,6 +2,13 @@
 cd ~
 sudo apt update
 
+# Desktop(MATE)のインストール
+yes | sudo apt install ubuntu-desktop
+yes | sudo apt install mate-*
+sudo apt install ubuntu-mate-*
+echo mate-session > ~/.xsession
+# ここでYとok、lightGBMを選ぶ
+
 # リモートデスクトップのインストール
 yes | sudo apt install xrdp
 sudo update-rc.d -f xrdp defaults
